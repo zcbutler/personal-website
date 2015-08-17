e4898369dd96c0bff5989b80086afe262a214d05
Zach Butler <zcbutler@ncsu.edu>
2015-08-17 02:35:25 -0400

    Style revision
    1. Changed background video to clearer, HQ video that looks nicer
    2. Navigation bar now changes color when the window scrollTop passes the
    header
    3. The background colors on the sections were changed to make more sense
    and to draw more attention to the content than the background
    4. Credit was given to the video owner in the footer
    5. Code refactor was done in index.js
    6. All information will reside in index.html. The website is this single
    page
    7. GitHub and LinkedIn links are no longer dummy and go to the correct
    locations
    8. A GitHub Tour section skeleton was added. The section will give
    detailed information about each repository I contribute to or personally
    own on github

--- src/js/index.js
+++ src/js/index.js
@@ -1,94 +1,89 @@
+var setNavBackground = function () {
+    var scrollTop = $(window).scrollTop();
+    var minScrollToBg = $('header').outerHeight();
+    if (scrollTop > minScrollToBg) {            //We scrolled down far enough
+        $('nav').css({
+            'z-index': '10',
+            'background': '#FFFFFF'
+        });
+    } else {                                    //We scrolled back up
+        $('nav').css({
+            'background': 'transparent'
+        });
+    }
+};
+
+var setCopyrightDate = function() {
+    var today = new Date();
+    $('span#copyright').html(today.getFullYear());
+};
+
+var resizeBackgroundVideo = function(width, height) {
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+    var widthToHeight = {
+        video: width / height,
+        window: windowWidth / windowHeight
+    };
+    var video = $('video#background');
+    if (widthToHeight.window > widthToHeight.video) {   //Width too small
+        var currentDisplayWidth = width * (windowHeight / height);
+        var upscaleX = windowWidth / currentDisplayWidth;
+        video.css({
+            '-webkit-transform': 'scaleX(' + upscaleX + ')',
+            '-ms-transform': 'scaleX(' + upscaleX + ')',
+            'transform': 'scaleX(' + upscaleX + ')'
+        });
+    } else {                                            //Height too small
+        var currentDisplayHeight = height * (windowWidth / width);
+        var upscaleY = windowHeight / currentDisplayHeight;
+        video.css({
+            '-webkit-transform': 'scaleY(' + upscaleY + ')',
+            '-ms-transform': 'scaleY(' + upscaleY + ')',
+            'transform': 'scaleY(' + upscaleY + ')'
+        });
+    }
+};
+
+var scrollToSection = function(href) {
+    var pos = $(href).offset().top - $('nav').outerHeight();
+    $('body').animate({
+        scrollTop: pos
+    }, 'slow', function() {
+        window.location.hash = href;
+    });
+};
+
 (function() {
     angular.module('zachButler', ['ui.bootstrap'])
 
     .controller('MainController', function($scope) {
-        window.my_scope = $scope;
 
-        $scope.tabs = [
-            {
-                heading: 'About',
-                active: false,
-                url: './templates/about.html',
-                img: './img/black.gif'
-            },{
-                heading: 'Work Experience',
-                active: false,
-                url: './templates/work-experience.html',
-                img: './img/black.gif'
-            },{
-                heading: 'Education',
-                active: false,
-                url: './templates/education.html',
-                img: './img/black.gif'
-            }
-        ];
-
-        $scope.routePage = function(url) {
-            window.location = url;
-        };
     });
 })();
 
-
 $(document).ready(function() {
-    var today = new Date();
-    $('span#copyright').html(today.getFullYear());
 
-    var setNavBackground = function () {
-        var scrollTop = $(window).scrollTop();
-        var minScrollToBg = 10;
-        if (scrollTop > minScrollToBg) {            //We scrolled down far enough
-            $('nav').css({
-                'z-index': '10',
-                'background': '#FFFFFF',
-                'background-color': '#FFFFFF !important'
-            });
-        } else {                                    //We scrolled back up
-            $('nav').css({
-                'background': 'transparent !important',
-                'background-color': ''
-            });
-        }
-    };
-    setNavBackground();
-
-    $('video#background').bind('loadedmetadata', function () {
-        this.currentTime = 53;
-        var intrinsic = {
-            video: {
-                width: this.videoWidth,
-                height: this.videoHeight
-            },
-            window: {
-                width: $(window).width(),
-                height: $(window).height()
-            }
-        };
-        var widthToHeight = {
-            video: intrinsic.video.width / intrinsic.video.height,
-            window: intrinsic.window.width / intrinsic.window.height
-        };
-        if (widthToHeight.window > widthToHeight.video) {   //Width too small
-            var currentDisplayWidth = intrinsic.video.width * (intrinsic.window.height / intrinsic.video.height);
-            var upscaleX = intrinsic.window.width / currentDisplayWidth;
-            $(this).css({
-                '-webkit-transform': 'scaleX(' + upscaleX + ')',
-                '-ms-transform': 'scaleX(' + upscaleX + ')',
-                'transform': 'scaleX(' + upscaleX + ')'
-            });
-        } else {                                            //Height too small
-            var currentDisplayHeight = intrinsic.video.height * (intrinsic.window.width / intrinsic.video.width);
-            var upscaleY = intrinsic.window.height / currentDisplayHeight;
-            $(this).css({
-                '-webkit-transform': 'scaleY(' + upscaleY + ')',
-                '-ms-transform': 'scaleY(' + upscaleY + ')',
-                'transform': 'scaleY(' + upscaleY + ')'
-            });
-        }
-    });
+    setCopyrightDate();
 
     $(window).on('scroll', function () {
         setNavBackground();
+    });
+    setNavBackground();
+
+    $('video#background').bind('loadedmetadata', function() {
+        resizeBackgroundVideo(this.videoWidth, this.videoHeight);
+    });
+
+    $(window).on('resize', function() {
+        var video = document.getElementById('background');
+        resizeBackgroundVideo(video.videoWidth, video.videoHeight);
+    });
+
+    $('.scroll-item').on('click', function(e) {
+        e.preventDefault();
+        var href = $(this).attr('href');
+        scrollToSection(href);
     });
 
 });
